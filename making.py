72eff4eeee357dd30c9b7e64ed8c83119b870139
yongluck <asdcv0428@naver.com>
2017-11-24 14:07:20 +0900

    Add files via upload

--- making.py
+++ making.py
@@ -1,28 +1,54 @@
 
-import math, sys
+import sys, threading, math
 import pygame
 from pygame.locals import *
 
 pygame.init()
 
-screen = pygame.display.set_mode((1024, 768), DOUBLEBUF)
-#player = pygame.image.load("plane.png")
+screen = pygame.display.set_mode((1280, 1024), DOUBLEBUF)
 pygame.display.set_caption('THAAD')
-font = pygame.font.Font("")
-
+clock = pygame.time.Clock()
 
 FPS = 60
+background = pygame.image.load('back.jpg')
+
 
 class Character ():
-    def __init__(self, image, loc):
+    def __init__(self, image, locx, locy):
         self.image = pygame.image.load(image)
-        self.loc = loc
+        self.locx = locx
+        self.locy = locy
+
+    def launch(self):
+        pass
+
+class Enemy ():
+    def __init__(self, image, locx, locy, angle):
+        self.image = pygame.image.load(image)
+        self.locx = locx
+        self.locy = locy
+        self.angle = angle
 
 
-thaad = Character("plane.png", 100)
+    def transform_angle(self):
+        while True:
+            self.angle = self.angle + 0.1
+            self.image = pygame.transform.rotate(self.image, self.angle)
+             9j 0
+
+
+
+thaad = Character('THAAD.jpg', 100, 500)
+lv1 = Enemy('plane.png', 900, 200, 0)
+
+
+
+
 
 while True:
     screen.fill((255, 255, 255))
+    screen.blit(background,(0,0))
+
 
     events = pygame.event.get()
     for event in events:
@@ -30,11 +56,19 @@ while True:
             pygame.quit()
             sys.exit()
 
- #       if pygame.key.get_pressed()[K_RIGHT]:
+    if pygame.key.get_pressed()[K_RIGHT]:
+        thaad.locx = thaad.locx + 10
+
+    if pygame.key.get_pressed()[K_LEFT]:
+        thaad.locx = thaad.locx - 10
 
 
-    screen.blit(thaad, (100.,100))
+
+
+    screen.blit(thaad.image, (thaad.locx,thaad.locy))
+    screen.blit(lv1.image, (lv1.locx, lv1.locy))
+
 
     pygame.display.flip()
+    clock.tick(FPS)
 
-    clock = pygame.time.Clock()
