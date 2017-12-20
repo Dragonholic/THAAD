edc1c299a7d2a2164b35e82a2c2e109ee7557b25
GivenStar <33537901+GivenStar@users.noreply.github.com>
2017-12-20 19:03:12 +0900

    Add files via upload

--- making.py
+++ making.py
@@ -1,8 +1,9 @@
 import sys
-import threading
+import random
 import math
 import pygame
 from pygame.locals import *
+
 
 pygame.init()
 
@@ -10,10 +11,12 @@ screen = pygame.display.set_mode((1280, 1024), FULLSCREEN)
 pygame.display.set_caption('THAAD')
 clock = pygame.time.Clock()
 
+
 FPS = 60
 background = pygame.image.load('back.jpg')
 
 
+#게임 오브젝트 클래스
 class GameObject(object):
     locx = 0
     locy = 0
@@ -36,7 +39,9 @@ class GameObject(object):
         surface.blit(rot_image, rect)
 
 
-class Character(GameObject):
+
+#메인캐릭터 클래스
+class Character (GameObject):
     THAAD_MAIN = 'THAAD_MAIN.png'
     THAAD_TURN = 'thaad_turn.png'
 
@@ -93,22 +98,30 @@ class Character(GameObject):
         self.angle = max((- math.pi / 6, min((self.angle, math.pi / 2))))
 
 
-class Enemy(GameObject):
 
+
+#적 미사일 클래스
+class Enemy (GameObject):
     def __init__(self, image, locx, locy, angle):
         self.image = pygame.image.load(image)
         self.locx = locx
         self.locy = locy
         self.angle = angle
+ #      self.alpha = alpha
         self.gravity = 0.5
+#        self.image = image.set_alpha(1)
 
-        resize_factor = 0.5
+        resize_factor = 0.8
 
         mw, mh = self.image.get_size()
         self.image = pygame.transform.scale(self.image, (int(mw * resize_factor), int(mh * resize_factor)))
 
+
+
         self.speed_x = math.cos(math.pi * 7 / 4 - self.angle) * 30
         self.speed_y = math.sin(math.pi * 7 / 4 - self.angle) * 30
+
+
 
     def do_update(self, events):
         self.locx -= self.speed_x
@@ -119,8 +132,8 @@ class Enemy(GameObject):
             kill(self)
 
 
-class Bullet(GameObject):
-
+#사드 탄환 클래스
+class Bullet (GameObject):
     def __init__(self, locx, locy, angle):
         self.locx = locx
         self.locy = locy
@@ -128,13 +141,16 @@ class Bullet(GameObject):
         self.image = pygame.image.load("park.png")
         self.gravity = 0.4
 
+        
         resize_factor = 0.4
 
         mw, mh = self.image.get_size()
         self.image = pygame.transform.scale(self.image, (int(mw * resize_factor), int(mh * resize_factor)))
 
+
         self.speed_x = math.cos(math.pi * 7 / 4 - self.angle) * 30
         self.speed_y = math.sin(math.pi * 7 / 4 - self.angle) * 30
+
 
     def do_update(self, events):
         self.locx += self.speed_x
@@ -144,29 +160,60 @@ class Bullet(GameObject):
         if self.locx > 1380 or self.locx < -100 or self.locy < -100 or self.locy > 1124:
             kill(self)
 
+
+
 game_objects = []
 
 
+#스폰 함수
 def spawn(game_object):
     game_objects.append(game_object)
     return game_object
 
-
+#제거 함수
 def kill(game_object):
     game_objects.remove(game_object)
     return game_object
 
 
+#사드 메인 캐릭터 스폰
 thaad = spawn(Character(130, 720))
-lv1 = spawn(Enemy('bigbig.png', 1200, 700, 0))
-t = 0
 
+
+
+# 적 미사일 종류별 스폰 함수
+def lv1():
+    random_y = random.randrange(500,800)
+    spawn(Enemy('lodong1.png', 1200, random_y, 0))
+
+def lv2():
+    random_y = random.randrange(500,800)
+    spawn(Enemy('lodong2.png', 1200, random_y, 0))
+def lv3():
+    random_y = random.randrange(500,800)
+    spawn(Enemy('lodong3.png', 1200, random_y, 0))
+def lv4():
+    random_y = random.randrange(500,800)
+    spawn(Enemy('lodong4.png', 1200, random_y, 0))
+def lv5():
+    random_y = random.randrange(500,800)
+    spawn(Enemy('lodong5.png', 1200, random_y, 0))
+
+
+t=0
+
+
+#실행
 while True:
     screen.fill((255, 255, 255))
     screen.blit(background, (0, 0))
 
+
+
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
+
+
 
     events = pygame.event.get()
     for event in events:
@@ -180,23 +227,25 @@ while True:
         entity.draw(screen)
 
 
+    if t%30 == 0:
+        lv1()
 
-
-
+#게임 종료 버튼
     if 1230+50 > mouse[0] > 1230 and 0+50 > mouse[1] > 0 :
-
         pygame.draw.rect(screen, (255, 0, 0), (1230, 0, 50, 50))
 
         if click[0]:
-            pygame.quit()
-            sys.exit()
+             pygame.quit()
+             sys.exit()
     else:
         pygame.draw.rect(screen, (155, 0, 0), (1230, 0, 50, 50))
 
     font = pygame.font.Font("arialbd.ttf", 40)
-    text = font.render("X", True, (0, 0, 0), (1242, 3))
-    screen.blit(text, (1242, 3))
+    text = font.render("X", True, (0,0,0), (1242,3))
+    screen.blit(text,(1242,3))
+
 
     pygame.display.flip()
     t += 1
     clock.tick(FPS)
+
